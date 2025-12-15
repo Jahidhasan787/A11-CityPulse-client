1a0bfb76f1744d1bff303813dec7af69a0e22848
Jahid <jahidict56@gmail.com>
2025-12-15 12:57:48 +0600

    added leatest section

--- src/Pages/Home/LatestResolve.jsx
+++ src/Pages/Home/LatestResolve.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import IssueCard from "../../Component/IssueCard";
 
 const LatestResolve = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    {
+      fetch(`http://localhost:3000/issues`)
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data);
+        });
+    }
+  }, []);
+
   return (
     <div>
       <h2 className="text-4xl mt-16 mb-3 text-center font-bold ">
@@ -9,6 +21,11 @@ const LatestResolve = () => {
       <p className="text-gray-600 text-center">
         Recently resolved public issue with confirmed solution.
       </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 my-10">
+        {data.map((issue) => (
+          <IssueCard key={issue._id} issue={issue}></IssueCard>
+        ))}
+      </div>
     </div>
   );
 };
