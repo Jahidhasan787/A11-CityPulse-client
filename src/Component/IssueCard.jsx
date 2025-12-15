732c29405e6a71e1d343ee7def6dcab37938b43e
Jahid <jahidict56@gmail.com>
2025-12-15 14:00:09 +0600

    added leatest section

--- src/Component/IssueCard.jsx
+++ src/Component/IssueCard.jsx
@@ -5,16 +5,16 @@ const IssueCard = ({ issue }) => {
   const { _id, image, category, title, location, amount } = issue;
   return (
     <div>
-      <div className="flex flex-col justify-between card bg-base-100 h-[450px] shadow-sm border border-gray-100">
+      <div className="flex flex-col justify-between p-5 rounded-xl  bg-base-300 h-full shadow-sm border  border-gray-100">
         <figure>
           <img
             src={image}
             alt="plant image"
-            className="w-[300px] h-[250px] rounded-2xl py-3 mt-10"
+            className="w-[380px] h-[250px] rounded-xl  "
           />
         </figure>
-        <div className="card-body">
-          <div className="flex justify-between items-center">
+        <div className="">
+          <div className="flex justify-between gap-2 items-center">
             <h2 className="card-title">{title}</h2>
             <span className="bg-green-200 rounded px-2 py-1">{category}</span>
           </div>
@@ -23,9 +23,9 @@ const IssueCard = ({ issue }) => {
             <p className="font-bold ">Amount: ${amount}</p>
           </div>
 
-          <div className="card-actions ">
-            <Link to={''}>
-              <button className="btn btn-primary">See Details</button>
+          <div className=" ">
+            <Link to={""}>
+              <button className="btn w-full btn-primary ">View</button>
             </Link>
           </div>
         </div>
--- src/Pages/Home/Category.jsx
+++ src/Pages/Home/Category.jsx
@@ -6,7 +6,10 @@ const Category = () => {
   return (
     <div>
       <div className="mt-16 mx-auto">
-        <h2 className="text-4xl mb-5 text-center font-bold ">Issue Category</h2>
+        <h2 className="text-4xl mb-2 text-center font-bold ">Issue Category</h2>
+        <p className="text-gray-600 text-center mb-8">
+        Filter issues by category for better organization and faster access.
+      </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-5">
           <div className="p-5 border rounded-xl shadow-sm hover:shadow-md transition cursor-pointer">
             <p className="text-4xl text-blue-600 mb-2">
